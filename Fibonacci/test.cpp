1c1084b2895653f96a03c1604eee3fb3575d34db
Kalteplatte <david.steffen@uni-jena.de>
2014-11-10 10:17:27 +0100

    testfile to test any fibonaccinumber

--- Fibonacci/test.cpp
+++ Fibonacci/test.cpp
@@ -2,71 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
-unsigned long Fibonacci1 (int n){        //recursive function
-	if (n<=1)	return n;
 
-	return Fibonacci1(n-1)+Fibonacci1(n-2);
-}
+unsigned long Fibonacci1 (int n);
 
 
+unsigned long Fibonacci2 (int n);
+
+
+unsigned long Fibonacci3 (int n);
+vector<vector<double>> IdentityM(vector<vector<double>> v);
 
+vector<vector<double> > MatrixMult(vector<vector<double> > v, vector<vector<double> > w);
 
-unsigned long Fibonacci2 (int n){      //function with linear time and linear memory
-	if(n<=1)	return n;
-	vector <int> fib (n);
-	fib[0]=0;
-	fib[1]=1;
-	int i=2;
+vector<vector<double> > ExponentiationBySquaring(vector<vector<double> > v, double n);
 
-	while(i<(n+1)){
-		fib[i]=fib[i-1]+fib[i-2];
-		i++;
-	}
-	return fib[n-1];
-}
+unsigned long Fibonacci4(int n);
 
+double sExponentiationBySquaring(double x, int n);
 
+unsigned long Fibonacci5(int n);
 
-
-unsigned long Fibonacci3 (int n){  //function with linear time and constant memory
-	if(n<=1)	return n;
-	int fib [3];
-	fib[0]=0;
-	fib[1]=1;
-	int i=2;
-	while(i<n+1){
-		fib[2]=fib[1]+fib[0];
-		fib[0]=fib[1];
-		fib[1]=fib[2];
-		i++;
-	}
-	return fib[2];
-}
-
-void test(){
-if (Fibonacci1(1)!=1) std::cout << "Fehler bei Fibonacci1 für n=1!";
-if (Fibonacci1(10)!=55) std::cout << "Fehler bei Fibonacci1 für n=10!";
-if (Fibonacci1(25)!=75025) std::cout << "Fehler bei Fibonacci1 für n=25!";
-if (Fibonacci1(43)!=433494437) std::cout << "Fehler bei Fibonacci1 für n=43!";
-if (Fibonacci2(1)!=1) std::cout << "Fehler bei Fibonacci2 für n=1!";
-if (Fibonacci2(10)!=55) std::cout << "Fehler bei Fibonacci2 für n=10!";
-if (Fibonacci2(25)!=75025) std::cout << "Fehler bei Fibonacci2 für n=25!";
-if (Fibonacci2(43)!=433494437) std::cout << "Fehler bei Fibonacci2 für n=43!";
-if (Fibonacci3(1)!=1) std::cout << "Fehler bei Fibonacci3 für n=1!";
-if (Fibonacci3(10)!=55) std::cout << "Fehler bei Fibonacci3 für n=10!";
-if (Fibonacci3(25)!=75025) std::cout << "Fehler bei Fibonacci3 für n=25!";
-if (Fibonacci3(43)!=433494437) std::cout << "Fehler bei Fibonacci3 für n=43!";
-}
-
-
-
-
-int main(){
-test();
-return 0;
-}
-
-
-
+unsigned long Fibonacci6(int n);
+
+int main (){	int n;                /* test various numbers n */	cin >> n;	assert(Fibonacci1(n)==Fibonacci6(n));	assert(Fibonacci2(n)==Fibonacci6(n));	assert(Fibonacci3(n)==Fibonacci6(n));	assert(Fibonacci4(n)==Fibonacci6(n));	assert(Fibonacci5(n)==Fibonacci6(n));	int x=0;               /* Input so that the window stays */	cin >> x;	return 0;}
